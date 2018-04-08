e24eb2fdf6bf58afce58a09ade2315ed0736fba7
katycarr <khcarr9@gmail.com>
2018-04-07 21:11:07 -0400

    add images to article serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,4 +1,4 @@
 class ArticleSerializer < ActiveModel::Serializer
   has_many :topics
-  attributes :id, :title, :author, :source, :published_at, :description, :url
+  attributes :id, :title, :author, :source, :published_at, :description, :url, :img_url
 end
