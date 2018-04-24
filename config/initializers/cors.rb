c9d48f1f94756396ddd44c2e2e1d5a5c3b806d60
katycarr <khcarr9@gmail.com>
2018-04-24 11:39:52 -0400

    cors for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,6 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins 'frontpage-app.herokuapp.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
     origins 'localhost:3000'
 
     resource '*',
