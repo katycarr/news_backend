89c5eb7ff93aa8a94a28e42615405b2388469b86
katycarr <khcarr9@gmail.com>
2018-04-26 11:41:50 -0400

    cleaner

--- app/classes/clean.rb
+++ app/classes/clean.rb
@@ -3,9 +3,12 @@ class Clean
 
   def clear_old
     old_articles = Article.all.select do |article|
-      article.published_at < 1.week.ago
+      article.published_at < 2.days.ago
     end
-    Article.delete(id: old_articles.map(&:id))
-    ArticleTopic.delete(article_id: old_articles.map(&:id))
+    Article.where(id: old_articles.map(&:id)).delete_all
+    old_article_topics = ArticleTopic.where(article_id: old_articles.map(&:id))
+
+    Topic.where(id: old_article_topics.map(&:topic_id)).delete_all
+    ArticleTopic.where(id: old_article_topics.map(&:id)).delete_all
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,12 @@ class Topic < ApplicationRecord
 
   has_many :article_topics
   has_many :articles, through: :article_topics
+
+  def article_count
+    ArticleTopic.where(topic_id: self.id).count
+  end
+
+  def user_count
+    UserTopic.where(topic_id: self.id).count
+  end
 end
